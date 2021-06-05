63b3f01e1d872a269c15aa18e3a8fcae1668ca06
daisuke8000 <55035595+daisuke8000@users.noreply.github.com>
2021-06-06 03:34:21 +0900

    base-header add (#5)

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,6 +5,8 @@ import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoute";
 import { Page404 } from "../components/pages/Page404";
 
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+
 export const Router: VFC = memo(() => {
     return (
         <Switch>
@@ -24,7 +26,9 @@ export const Router: VFC = memo(() => {
                                exact={route.exact}
                                path={`${url}${route.path}`}
                            >
-                               {route.children}
+                               <HeaderLayout>
+                                   {route.children}
+                               </HeaderLayout>
                            </Route>
                         ))}
                     </Switch>
