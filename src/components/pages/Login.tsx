925950cc31e3e4fd80e17bb9203a858bb3886887
daisuke8000 <daisuke12108000@gmail.com>
2021-06-14 08:48:56 +0900

    login form add

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,62 @@
-import {memo, VFC} from "react";
+import React, {memo, VFC, useState, ChangeEvent} from "react";
+import { Flex, Box, Heading, Divider, Input, Stack, Text } from "@chakra-ui/react";
+import { PrimaryButton } from "../atoms/PrimaryButton";
+import { SignUpButton } from "../atoms/SignUpButton";
 
 export const Login: VFC = memo(() => {
-   return (
-       <p>LoginPage</p>
+
+    const [mailAddress, setMailAddress] = useState<string>('');
+    const [password, setPassWord] = useState<string>('');
+    const [changeMode, setChangeMode] = useState<boolean>(true);
+
+    //textboxの型指定は覚えるだけ
+    const onChangeMailAddress = (e:ChangeEvent<HTMLInputElement>) => {
+        setMailAddress(e.target.value);
+    }
+    const onChangePassWord = (e:ChangeEvent<HTMLInputElement>) => {
+        setPassWord(e.target.value);
+    }
+
+    return (
+       <Flex align="center" justify="center" height="100vh">
+          <Box bg="orange.100" w="sm" p={5} borderRadius="md" shadow="md">
+              { changeMode ?
+                  <>
+                    <Heading as="h1" size="lg" textAlign="center" color="blue.500">FourLineDiary</Heading>
+                    <Divider my={4} borderColor="blue.500" />
+                  </>
+                  :
+                  <>
+                    <Heading as="h1" size="lg" textAlign="center" color="teal.500">FourLineDiary</Heading>
+                    <Divider my={4} borderColor="teal.500" />
+                  </>
+
+              }
+              <Stack spacing={3} py={5} px={10}>
+                  { changeMode ?
+                      <>
+                          <Input type="email" placeholder="mailAddress" value={mailAddress} onChange={onChangeMailAddress}/>
+                          <Input type="password" placeholder="password" value={password} onChange={onChangePassWord}/>
+                          <PrimaryButton>Signin</PrimaryButton>
+                      </>
+                      :
+                      <>
+                          <Input type="email" placeholder="mailAddress" value={mailAddress} onChange={onChangeMailAddress}/>
+                          <Input type="password" placeholder="password" value={password} onChange={onChangePassWord}/>
+                          <SignUpButton>SignUp</SignUpButton>
+                      </>
+                  }
+                  <Text
+                      textAlign="center"
+                      justify="center"
+                      color="gray.500"
+                      onClick={() => setChangeMode(!changeMode)}
+                      _hover={{ cursor: "pointer"}}
+                  >
+                      ChangeMode?
+                  </Text>
+              </Stack>
+          </Box>
+       </Flex>
    );
 });
